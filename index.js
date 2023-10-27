04b2e6742420505c54d0967d9d28a6e26ac3f102
VeroPolegre <veronicapolegre98@gmail.com>
2023-10-27 13:21:37 +0200

    routes added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const db = require("./config/database");
 const PORT = 3000;
 
+app.use(express.json());
+app.use("/categories", require("./routes/categories"));
+app.use("/products", require("./routes/products"));
+
 app.get("/createdb", (req, res) => {
   let sql = "CREATE DATABASE expressDB";
   db.query(sql, (err, result) => {
