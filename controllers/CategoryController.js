9cc5a0e1cf347f52ccdbc37de55a86bca8437128
VeroPolegre <veronicapolegre98@gmail.com>
2023-10-28 19:20:14 +0200

    added category routes and controller

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -10,7 +10,37 @@ const CategoryController = {
     db.query(sql, category, (err, result) => {
       if (err) throw err;
       console.log(result);
-      res.send("Category added!");
+      res.status(201).send("Category added!");
+    });
+  },
+
+  update(req, res) {
+    const categoryId = req.params.id;
+    const newName = req.body.name_category;
+    const newDescription = req.body.description;
+
+    let sql =
+      "UPDATE categories SET name_category = ?, description = ? WHERE id = ?";
+    db.query(sql, [newName, newDescription, categoryId], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.status(200).send(`Category ${newName} updated!!`);
+    });
+  },
+
+  getAll(req, res) {
+    let sql = "SELECT * FROM categories";
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(200).send(result);
+    });
+  },
+
+  getById(req, res) {
+    let sql = `SELECT * FROM categories WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(200).send(result);
     });
   },
 };
--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,9 @@ const express = require("express");
 const CategoryController = require("../controllers/CategoryController.js");
 const router = express.Router();
 
-router.post("/categories/create", CategoryController.create);
+router.post("/create", CategoryController.create);
+router.put("/update/:id", CategoryController.update);
+router.get("/", CategoryController.getAll);
+router.get("/id/:id", CategoryController.getById);
 
 module.exports = router;
